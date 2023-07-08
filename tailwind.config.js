01fdb6b76b8a06e7b971da129c7a96291fdd4a23
ahmed <elshehry@gamil.com>
2023-07-08 23:52:45 +0300

    Finished all features

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    container:{
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
+  },
+  plugins: [],
+}
+
